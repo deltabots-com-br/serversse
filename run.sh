393df5436d90a4ecf84ba159fac74909ef973bb6
Adriano Oliveira | 2BX <140265935+adriano2bx@users.noreply.github.com>
2025-10-28 07:29:37 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-# run.sh (Versão aprimorada usando ENVs)
-
-# Lê as variáveis de ambiente, com fallbacks seguros
-PORT=${APP_PORT:-8000}
-WORKERS=${UVICORN_WORKERS:-1} # 1 como um fallback seguro se a variável não for definida
-
-echo "Iniciando servidor FastAPI com Uvicorn na porta $PORT e $WORKERS workers..."
-
-# Executa o servidor ASGI Uvicorn
-uvicorn main:app --host 0.0.0.0 --port $PORT --workers $WORKERS
+uvicorn main:app --host 0.0.0.0 --port 8000
